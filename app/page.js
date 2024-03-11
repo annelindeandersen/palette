39cd5eff3dbdd608a0ec4a9f44021271f8271c99
Anne Linde <and@lruddannelse.dk>
2024-03-11 22:54:59 +0100

    Colour palette tested with blue & pink

--- app/page.js
+++ app/page.js
@@ -1,113 +1,122 @@
-import Image from "next/image";
+const baseColorPink = [342, 100, 80]; // HSL values
+const baseColorBlue = [185, 100, 28]; // HSL values
+
+// Hue is a degree on the color wheel from 0 to 360. 
+// 0 is red, 120 is green, and 240 is blue. 
+// Saturation is a percentage value. 
+// 0% means a shade of gray, and 100% is the full color.
+
+const adjustLighterColorHSL = (h, s, l, index) => {
+
+  // First figure out lightness to calc how much lightness to add or deduct:
+  // if(l >= 87 && l <= 95) {
+  //   h -= 1;
+  //   l += index;
+  // } else if (l < 87 && l >= 70) {
+  //   l += (index * 3);
+  // } else if (l < 70 && l > 50) {
+  //   l += (index * 5);
+  // } else if (l <= 50 && l >= 20) {
+  //   l += (index * 9);
+  //   l += (index * 12);
+  // } 
+  
+  // if (l <= 95 && l >= 81) {
+  // l += (index * (l / 15));
+  // } else if (l <= 80 && l >= 66) {
+  // l += (index * (l / 14));
+  // } else if (l <= 65 && l >= 46) {
+  // l += (index * (l / 8));
+  // } else if (l <= 45 && l >= 36) {
+  // l += (index * (l / 3));
+  // } else if (l <= 35 && l >= 16) {
+  // l += (index * (l / 2));
+  // } else if (l <= 15) {
+  // l += (index * l);
+  // }
+  // } else {
+
+  //   l += (index * (l / 3));
+  // }
+
+  const diff = 100 - l;
+  l += (diff / 5.5) * index;
+  l = Math.min(100, Math.max(0, l)); // Ensure lightness value stays within 0-100 range
+
+  // if s === 0 it is grey and we should not touch saturation.
+  if(s !== 0) {
+    // if not red & pink
+    if(h < 300 && h > 10) {
+      s -= (index + 2) * 10;
+    }
+    s = Math.min(100, Math.max(1, s)); // Ensure lightness value stays within 1-100 range
+  }
+
+  return `hsl(${Math.ceil(h)}, ${Math.ceil(s)}%, ${Math.ceil(l)}%)`;
+};
+
+const adjustDarkerColorHSL = (h, s, l, index) => {
+
+  // First figure out lightness to calc how much lightness to add or deduct:
+  // if(l >= 10 && l <= 5) {
+  //   // if lightness is 5-10, take the hue up one and set lightness to - index.
+  //   h += 1;
+  //   l -= index;
+  // } else if(l === 0) {
+  //     // if lightness is 0, take the hue down one and set lightness to 50.
+  //     h += 1;
+  //     l = 10 / index;
+  //   } else {
+  //   l -= (index * 10);
+  // }
+
+  const diff = 100 - l;
+  
+  if(diff < 25) {
+    l -= (diff / 2) * index;
+  } else {
+    l -= (index * 3);
+  }
+  l = Math.min(100, Math.max(0, l)); // Ensure lightness value stays within 0-100 range
+
+  // if s === 0 it is grey and we should not touch saturation.
+  // if initial lightness is above or equals 25, decrease saturation on darker warm colors
+  if(s !== 0 && l >= 25) {
+    // if red/pink toned
+    if(h >= 300 || h <= 10) {
+      s -= ((index + 3) * 10);
+    } else {
+      s -= ((index + 2) * 5);
+    }
+    s = Math.min(100, Math.max(1, s)); // Ensure lightness value stays within 1-100 range
+  }
+
+  return `hsl(${Math.ceil(h)}, ${Math.ceil(s)}%, ${Math.ceil(l)}%)`;
+};
+
+
+const getPalette = (baseColor) => {
+  const lightGradientArray = Array.from(Array(6), (_, i) => adjustLighterColorHSL(baseColor[0], baseColor[1], baseColor[2], i)).reverse();
+  const darkGradientArray = Array.from(Array(5), (_, i) => adjustDarkerColorHSL(baseColor[0], baseColor[1], baseColor[2], i + 1));
+
+  return [...lightGradientArray, ...darkGradientArray]
+}
 
 export default function Home() {
+  const paletteBlue = getPalette(baseColorBlue);
+  const palettePink = getPalette(baseColorPink);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col items-center justify-between px-10 py-20 bg-white">
+      <h1 className="text-black">Colors:</h1>
+      <section className="grid gap-4 items-center">
+        <div className="flex flex-wrap gap-3 text-xs text-black">
+          {paletteBlue.map(item => <div className="h-32 w-36 flex flex-col-reverse rounded shadow-lg overflow-hidden" style={{background: item}}><span className="bg-white p-2">{item}</span></div>)}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex flex-wrap gap-3 text-xs text-black">
+          {palettePink.map(item => <div className="h-32 w-36 flex flex-col-reverse rounded shadow-lg overflow-hidden" style={{background: item}}><span className="bg-white p-2">{item}</span></div>)}
+        </div>
+      </section>
     </main>
   );
 }
