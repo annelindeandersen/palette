221d77db8cf8fd7a81adc8de530c4ac1139053c2
Anne Linde <and@lruddannelse.dk>
2024-03-14 13:59:42 +0100

    Spelling error and added border

--- app/components/PalettePicker.js
+++ app/components/PalettePicker.js
@@ -8,7 +8,7 @@ export const PalettePicker = () => {
     const [color, setColor] = useState(undefined);
     const HSLColor = getHSL(color);
 
-    const paletteCostum = color ? getPalette(HSLColor) : ''
+    const paletteCustom = color ? getPalette(HSLColor) : ''
     const paletteBlue = getPalette([185, 100, 28]);
     const palettePink = getPalette([342, 100, 80]);
 
@@ -16,13 +16,13 @@ export const PalettePicker = () => {
     <>
         <div className="flex gap-5 py-3 items-center">
             <h1 className="text-black text-4xl">Color palette picker:</h1>
-            <span className="rounded-full overflow-hidden h-10 w-10 flex items-center justify-center border">
+            <span className="rounded-full overflow-hidden h-10 w-10 flex items-center justify-center border-black border-2">
                 <input type="color" value={color} onChange={(e) => setColor(e.target.value)} className="h-20 shrink-0"/>
             </span>
         </div>
         <section className="grid gap-4 items-center">
             <div className="flex flex-wrap gap-3 text-xs text-black">
-                {color && paletteCostum.map(item => <div key={item} className="h-32 w-36 flex flex-col-reverse rounded shadow-lg overflow-hidden" style={{background: item}}><span className="bg-white p-2">{item}</span></div>)}
+                {paletteCustom && paletteCustom.map(item => <div key={item} className="h-32 w-36 flex flex-col-reverse rounded shadow-lg overflow-hidden" style={{background: item}}><span className="bg-white p-2">{item}</span></div>)}
             </div>
             <div className="flex flex-wrap gap-3 text-xs text-black">
                 {paletteBlue.map(item => <div key={item} className="h-32 w-36 flex flex-col-reverse rounded shadow-lg overflow-hidden" style={{background: item}}><span className="bg-white p-2">{item}</span></div>)}
